ff73a39367482f65b5dbb5b0cc91299aed9cf913
Ramon Carrion <ramoncarrion@gmail.com>
2020-09-30 18:43:55 +0200

    primer commit
    primer commit

---
+++ app/src/main/java/com/ramoncarrion/petagram/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ramoncarrion.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
